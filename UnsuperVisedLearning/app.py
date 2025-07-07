7f59bce563425f52a752abca44826720e8483cfd
Mohit Sharma <starcollect01@gmail.com>
2025-07-07 12:22:00 +0530

    new

---
+++ UnsuperVisedLearning/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, url_for
+import joblib
+import os
+
+app = Flask(__name__)
+
+# Load models
+scalar = joblib.load('scalar.lb')
+KMn = joblib.load("crop_recommendation_model.lb")
+df = joblib.load("Crop_recommendation_data.lb")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            N = int(request.form['nitrogen'])
+            P = int(request.form['phosphorus'])
+            K = int(request.form['potassium'])
+            temperature = float(request.form['temperature'])
+            humidity = float(request.form['humidity'])
+            ph = float(request.form['ph'])
+            rainfall = float(request.form['rainfall'])
+
+            user_data = [[N, P, K, temperature, humidity, ph, rainfall]]
+            trans_data = scalar.transform(user_data)
+            prediction = KMn.predict(trans_data)[0]
+
+            # Check which crops are most likely from the cluster
+            dt = dict(df[df['cluster_8'] == prediction]['label'].value_counts())
+
+            print("Predicted Crops Dictionary:", dt)
+
+            return render_template('index.html', dt=dt)
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
